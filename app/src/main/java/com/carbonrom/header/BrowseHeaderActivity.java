8d7e9474032925e5164837c4b255a22d69d94924
Adin Kwok <adin.kwok@carbonrom.org>
2017-01-22 16:22:34 -0800

    Change build environment
    *based off of mazda--'s previous commit
    
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>

--- app/src/main/java/com/carbonrom/header/BrowseHeaderActivity.java
+++ app/src/main/java/com/carbonrom/header/BrowseHeaderActivity.java
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-package com.dirtyunicorns.headers;
+package com.carbonrom.header;
 
 import android.app.ActionBar;
 import android.app.Activity;
